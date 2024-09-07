66ee0e0e802d38c74fddbea113aa139c3b264a45
leekh2002 <lkh4342@naver.com>
2024-09-07 15:26:55 +0900

    asef

---
+++ public/scripts.js
@@ -0,0 +1,157 @@
+const category = document.getElementById('courseCategory');
+const department = document.getElementById('departments');
+
+document.addEventListener('DOMContentLoaded', function () {});
+
+const categoryAndDeptfromdb = (category, department) => {
+  fetch('/categoryanddeptfromdb', {
+    method: 'GET',
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      data.forEach((element) => {
+        var option = document.createElement('option');
+        option.value = element.major1;
+        department.appendChild(option);
+      });
+    });
+};
+categoryAndDeptfromdb(category, department);
+
+document
+  .getElementById('loadTimetable')
+  .addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const timetableList = document.getElementById('timetableList');
+    const container = document.querySelector('.container');
+
+    timetableList.classList.toggle('show');
+
+    // 시간표가 열릴 때와 닫힐 때 container의 위치를 조정
+    container.classList.add('shift-left');
+  });
+document
+  .querySelector('.close-timetable')
+  .addEventListener('click', function () {
+    const timetableList = document.getElementById('timetableList');
+    timetableList.classList.remove('show');
+    const container = document.querySelector('.container');
+    container.classList.remove('shift-left');
+  });
+const searchForm = document.getElementById('searchForm');
+
+function adjustTableWidth() {
+  const thead = document.querySelector('.results-section .output_result thead');
+  const tbody = document.querySelector('.results-section .output_result tbody');
+  const element = document.querySelector('.output_result');
+  const scrollbarWidth = 17;
+
+  console.log('height: ', element.scrollHeight);
+  if (element.scrollHeight >= 400) {
+    console.log('height: ', element.scrollHeight);
+    thead.style.width = 'calc(100% - ' + scrollbarWidth + 'px)';
+  } else {
+    thead.style.width = '100%';
+  }
+  tbody.style.width = '100%'; // tbody는 항상 100% 너비를 유지
+  console.log('asefsaeiohf;seaoidjaelsifh');
+}
+
+document.getElementById('searchForm').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(searchForm);
+  const inst_method = document.getElementById('courseType').value;
+  const course_class = document.getElementById('courseCategory').value;
+  const prof_name = document.getElementById('prof_name').value;
+  const major = document.getElementById('department').value;
+  const name = document.getElementById('courseNumber').value;
+  const tbody = document.querySelector('.output_result tbody');
+  //console.log('tbody:',tbody);
+  //console.log(name, major);
+  tbody.replaceChildren();
+
+  const queryString = `inst_method=${encodeURIComponent(
+    inst_method
+  )}&course_class=${encodeURIComponent(
+    course_class
+  )}&prof_name=${encodeURIComponent(prof_name)}&major=${encodeURIComponent(
+    major
+  )}&name=${encodeURIComponent(name)}`;
+
+  console.log(formData);
+  fetch(`/process/search?${queryString}`, {
+    method: 'GET',
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data);
+      data.forEach((element) => {
+        const keys = Object.keys(element);
+        const newRow = document.createElement('tr');
+        // console.log('keys: ',keys)
+        keys.forEach((key) => {
+          const newCell = document.createElement('td');
+          newCell.textContent = element[key];
+          newRow.appendChild(newCell);
+        });
+        const addButton = document.createElement('button');
+        addButton.className += 'add-btn';
+        addButton.textContent = '+ 추가';
+        newRow.appendChild(addButton);
+        newRow.addEventListener('mouseenter', (event) => {
+          addTime(event);
+        });
+        //console.log('newRow: ',newRow);
+        tbody.appendChild(newRow);
+      });
+      adjustTableWidth();
+    });
+});
+
+function addTime(event) {
+  // console.log('element:', event.target);
+  const tdElements = event.target.querySelectorAll('td');
+  const name = tdElements[4].textContent;
+  const prof_name = tdElements[7].textContent;
+
+  // console.log(tdElements[2].textContent);
+  fetch(
+    `/process/getTime?sid=${tdElements[2].textContent}&class=${tdElements[3].textContent}`,
+    {
+      method: 'GET',
+    }
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      const dayDict = {};
+      dayDict['월'] = '1';
+      dayDict['화'] = '2';
+      dayDict['수'] = '3';
+      dayDict['목'] = '4';
+      dayDict['금'] = '5';
+      dayDict['토'] = '6';
+      dayDict['일'] = '7';
+      data.forEach((element) => {
+        console.log(element.day);
+        const column = document
+          .querySelector(`[data-day='${dayDict[element.day]}']`)
+          .getElementsByClassName('cols')[0];
+        const div=document.createElement('div');
+        div.style.position='absolute';
+        div.style.width='113.31px';
+        div.className+='subject ';
+        div.className+='color3';
+
+        //-------------내일 처리해야할거--------------
+        // div.style.top='0px';
+        // div.style.height='47.42px';
+        //--------------------------------------------
+        column.appendChild(div)
+        console.log(column);
+      });
+    });
+}
---
+++ svr.js
@@ -0,0 +1,181 @@
+const express = require('express');
+const mysql = require('mysql2');
+const path = require('path');
+const static = require('serve-static');
+const dbconfig = require('./config/dbconfig.json');
+const { parseArgs } = require('util');
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: dbconfig.host,
+  user: dbconfig.user,
+  password: dbconfig.password,
+  database: dbconfig.database,
+  multipleStatements: true,
+  debug: false,
+});
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/public', static(path.join(__dirname, 'public')));
+
+app.get('/categoryanddeptfromdb', (req, res) => {
+  console.log('categoryanddeptfromdb호출');
+
+  pool.getConnection((err, conn) => {
+    if (err) {
+      conn.release();
+      console.log('Mysql getConnection error. aborted');
+      res.json('<h1>db server 연결실패</h1>');
+      return;
+    }
+
+    console.log('데이터베이스 연결');
+
+    const sql1 = `
+    create temporary table temp_major
+    select es.major1
+    from
+    (
+      select distinct substring_index(substring_index(major, " ", numbers.n), " " , 10) as major1
+          from(
+              select 1 as n union all
+              select 2 union all
+              select 3 union all
+              select 4 union all
+              select 5 union all
+              select 6 union all
+              select 7 union all
+              select 8 union all
+              select 9 union all
+              select 10) as numbers
+              right outer join lecture
+              on char_length(major) - char_length(replace(major, " ", "")) >= numbers.n - 1
+              order by major1
+    ) as es;
+    `;
+    const sql2 =
+      'create temporary table temp_major2 as select * from temp_major;';
+    const sql3 =
+      'create temporary table temp_major3 as select * from temp_major; ';
+    const sql4 = `call majorloop(); `;
+    const sql5 = `delete from temp_major3 where major1='마이크로디그리과정 마이크로디그리과정 디지털'; `;
+    const sql6 = `select * from temp_major3; `;
+
+    const exec = conn.query(
+      sql1 + sql2 + sql3 + sql4 + sql5 + sql6,
+      (err, rows) => {
+        conn.release();
+        console.log('실행된 SQL: ' + exec.sql);
+
+        if (err) {
+          console.log('SQL 실행시 오류발생');
+          console.dir(err);
+          res.json('<h1>SQL query 실행실패</h1>');
+          return;
+        } else {
+          res.json(rows[5]);
+        }
+      }
+    );
+  });
+
+  //res.json('asgesafe');
+});
+
+app.get('/process/search', (req, res) => {
+  console.log('inst_method:', req.query.inst_method);
+  console.log('course_class:', req.query.course_class);
+  console.log('prof_name:', req.query.prof_name);
+  console.log('major:', req.query.major);
+  console.log('name:', req.query.name);
+  const inst_method = req.query.inst_method;
+  const course_class = req.query.course_class;
+  const prof_name = req.query.prof_name;
+  const major = req.query.major;
+  const name = req.query.name;
+  let where =
+    'where ' +
+    (course_class == '전체' ? '' : 'course_class = ? and ') +
+    (prof_name == '' ? '' : 'prof_name = ? and ') +
+    (major == '' || major == '전체' ? '' : 'major like ? and ') +
+    (name == ''
+      ? ''
+      : name.charAt(4) == '-' && name.charAt(9) == '-'
+      ? 'a.sid = ? and a.class = ? and'
+      : 'name like ? and ') +
+    (inst_method == '전체'
+      ? '(inst_method=' + '"원격수업"' + ' or ' + 'inst_method is null)'
+      : inst_method == '원격수업'
+      ? 'inst_method=' + '"원격수업"'
+      : 'inst_method is null');
+
+  console.log('where: ', where);
+  let params = [];
+  if (course_class != '전체') params.push(course_class);
+
+  if (prof_name != '') {
+    params.push(prof_name);
+  }
+
+  if (major != '' && major != '전체') params.push(major + '%');
+
+  if (name != '') {
+    console.log(
+      'name.charAt(4): ',
+      name.charAt(4),
+      'name.charAt(9)',
+      name.charAt(9)
+    );
+    if (name.charAt(4) == '-' && name.charAt(9) == '-') {
+      console.log('코드 검색');
+      params.push(name.substring(0, 9));
+      params.push(name.substring(10));
+    } else params.push('%' + name + '%');
+  }
+
+  console.log('params: ', params);
+  pool.getConnection((err, conn) => {
+    if (err) {
+      conn.release();
+      console.log('Mysql getConnection error. aborted');
+      res.writeHead('200', { 'Content-Type': 'text/html; charset=utf8' });
+      res.write('<h1>db server 연결실패</h1>');
+      res.end();
+      return;
+    }
+
+    console.log('데이터베이스 연결');
+    const exec = conn.query(
+      `select a.major, a.grade, a.sid, a.class, b.name, b.credit, a.course_class, a.prof_name, c.time, a.inst_method
+      from lecture as a
+      left join subject as b on a.sid=b.sid
+      left join (select sid, class, group_concat(concat(day,time,'(',place,')') separator ', ') as time
+             from timetable
+                 group by sid,class) as c on a.sid=c.sid and a.class=c.class ` +
+        where +
+        ';',
+      params,
+      (err, rows) => {
+        conn.release();
+        console.log('실행된 SQL: ' + exec.sql);
+
+        if (err) {
+          console.log('SQL 실행시 오류발생');
+          console.dir(err);
+          res.writeHead('200', { 'Content-Type': 'text/html; charset=utf8' });
+          res.write('<h1>SQL query 실행실패</h1>');
+          res.end();
+          return;
+        } else {
+          res.json(rows);
+        }
+      }
+    );
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
